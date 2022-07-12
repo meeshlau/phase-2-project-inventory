abccf356bfccedac4db5d25a55833fbeddea6a31
Michelle Lau <meeshklau@gmail.com>
2022-07-12 10:46:29 -1000

    add new container component

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React, { useEffect, useState } from "react"
+import InventoryContainer from "./InventoryContainer"
+
+function App() {
+  const [inventory, setInventory] = useState([])
+  console.log(inventory)
+  
+  useEffect(() => {
+    fetch("http://localhost:3000/equipment")
+    .then((r) => r.json())
+    .then((inv) => setInventory(inv))
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        <InventoryContainer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
